29058a9e8e4d1c6a758384d2574edca6de94aa70
Frank P <frank.pinder@dss.ca.gov>
2019-06-29 18:15:58 -0700

    updated the js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+
+//random letter generater 
 function randLetter() {
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var letter = letters[Math.floor(Math.random() * letters.length)];
@@ -5,3 +7,10 @@ function randLetter() {
     
 }
 
+//display the initial values 
+function start() {
+    winsText.textContent = "Wins " + wins;
+    lossesText.textContent = "Losses " + losses;
+    GuessLeftText.textContent = "Guess Left " + GuessLeft;
+    userChoiceText.textContent = "Your Guess so far " + ", " + Guesses.join(", ");
+}
